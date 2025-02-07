2b9bd1e30b2f5c05b776ba85e26b5702f417352a
JackCoder0 <jacksonxcontato13@gmail.com>
2025-02-06 23:02:28 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-center text-6xl font-bold">
+        Repositorio padrão com Tailwind CSS
+      </h1>
+    </div>
+  )
+}
